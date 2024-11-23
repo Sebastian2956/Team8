6d4990923d18a4650e6136cb14cb5a28867359c1
liam harvell <liam@harvell.us>
2024-11-22 23:49:24 -0500

    Set up automatic api token retrieval and an example function for making api requests (added the axios package)

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 require('dotenv').config({ path: '../.env' });
 const { ObjectId } = require('mongodb');
-
 //importing express, cors and body-parser
 const express = require('express'); //framework for server
 const cors = require('cors'); //Cross-Origin Resource Sharing, allows/restricts resources from being accessed by clients from different origins
 const bodyParser = require('body-parser'); //parses incoming request bodies in a middleware before your handlers, available under the req.body property
+const axios = require('axios')
 
 let tripId = "0";
 
@@ -17,6 +17,88 @@ client.connect();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+
+let tokenCache = {
+  accessToken: null,
+  expirationTime: null
+}
+
+const getAccessToken = async () => {
+
+  let isTokenExists = tokenCache.accessToken !== null && tokenCache.expirationTime !== null
+  let isTokenValid = Date.now() < tokenCache.expirationTime
+
+const credentials = `grant_type=${encodeURIComponent('client_credentials')}&` +
+                      `client_id=${encodeURIComponent(process.env.AMADEUS_CLIENT_ID)}&` +
+                      `client_secret=${encodeURIComponent(process.env.AMADEUS_CLIENT_SECRET)}`;
+
+  try {
+    console.log(tokenCache.accessToken)
+    console.log(tokenCache.expirationTime)
+    if (isTokenExists && isTokenValid) {
+      console.log("using current token")
+      return tokenCache.accessToken
+    }
+
+    const response = await axios.post('https://test.api.amadeus.com/v1/security/oauth2/token', credentials, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json'
+      },
+    })
+
+    const { access_token, expires_in } = response.data
+    tokenCache.accessToken = access_token;
+    tokenCache.expirationTime = Date.now() + expires_in * 1000;
+
+    console.log("returned token" , tokenCache.accessToken)
+    return tokenCache.accessToken
+  } catch (error) {
+    console.error('Error fetching token: ' + (error.response?.data || error.message))
+    throw new Error('Failed to authenticate with Amadeus API')
+  }
+}
+
+const findFlightsFromToWhereOnDate = async (origin, destination, date, adults) => {
+  try {
+
+    const token = await getAccessToken();
+    const api_url = 'https://test.api.amadeus.com/v2/shopping/flight-offers'
+
+    const response = await axios.get(api_url, {
+      params: {
+        originLocationCode: origin,
+        destinationLocationCode: destination,
+        departureDate: date,
+        adults: 1
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching flight-offers: ', (error.response?.data || error.message))
+    throw new Error('Failed to fetch flight-offers')
+  }
+}
+
+app.get('/api/findFlightsFromToWhereOnDate', async (req, res, next) => {
+  const { origin, destination, date } = req.query
+
+  if (!origin || !destination || !date) {
+    return res.status(400).json({ error: 'Missing required parameter(s)' })
+  }
+
+  try {
+    const flightData = await findFlightsFromToWhereOnDate(origin, destination, date)
+    res.json(flightData)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
 
 //req represents incoming request from client
 //res represents server's response to client
@@ -360,5 +442,6 @@ app.delete('/api/deleteHotel', async (req, res, next) => {
         res.status(500).json({ error: 'An error occurred while deleting the hotel' });
     }
 });
+
 
 app.listen(process.env.LOCALHOST_PORT || 3000);
