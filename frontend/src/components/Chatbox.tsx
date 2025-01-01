a7805eeacda6d647515722595a5b96fdbe51896c
Bentley Aing <rakkokey@Mac.lan1>
2025-01-01 15:14:44 -0500

    AI prompt messages now show in AI Tools box

--- frontend/src/components/Chatbox.tsx
+++ frontend/src/components/Chatbox.tsx
@@ -4,6 +4,7 @@ import { LOCALHOST_PORT } from "../config";
 function Chatbox(props:any){
     const [state, setState] = useState('');
     const [message, setMessage] = useState('');
+    const [messageList, setMessageList] = useState<any[]>([]);
 
     const [query, setQuery] = useState('');
 
@@ -15,6 +16,7 @@ function Chatbox(props:any){
         console.log(js);
 
         try{
+            
             const response = await fetch(LOCALHOST_PORT + '/api/ai/queryAI',{
                 method: 'POST', body:js, headers:{'Content-Type': 'application/json'}
             });
@@ -22,26 +24,51 @@ function Chatbox(props:any){
             const res = JSON.parse(txt);
             console.log(res);
             //res.candidates.content.parts.text
-            if(res.error.length > 0){
-                setMessage('API Error: ' + res.error);
-            }else{
-                setMessage('AI queried');
-            }
+            await setMessage(message => res.candidates[0].content.parts[0].text);
+            console.log(res.candidates[0].content.parts[0].text)
+            console.log(message);
+            
+            //calls function to append message to chatbox
+            
             
         }catch(error:any){
             setMessage(error.toString());
         }
 
     }
+
+
+    function addAIResponse(){
+        
+        setMessageList([
+            ...messageList,
+            {message: message}]); 
+
+        
+
+    }
+
     function handleQueryChange(e: any): void{
         setQuery(e.target.value);
     }
+
+    useEffect(() =>{
+        addAIResponse();
+    },[message]);
+    
 
 
     //check for state changes
     return(
         <div className="chatbox">
-            <form onSubmit={queryAI}>
+            <form onSubmit={(e) =>{
+                queryAI(e)
+            }}>
+                {
+                    messageList.map((message, index: number) =>(
+                        <p key={index} className="ai-message">{message.message}</p>
+                    ))
+                }
                 <input type="text"  onChange={handleQueryChange}></input>
             </form>
 
